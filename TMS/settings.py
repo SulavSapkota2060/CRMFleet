5a50d6868ea0d9066794575cc9fed398dc3651b9
Sulav Sapkota <sulavsapkota100@gmail.com>
2020-08-29 18:03:45 +0545

    Me

--- TMS/settings.py
+++ TMS/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -136,6 +137,7 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 import os
 
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
